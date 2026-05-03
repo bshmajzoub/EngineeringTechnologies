f5db53b8081e082f854a062e534bc4d664c0d7f5
Besher ElMajzoub <beshermajzou@gmail.com>
2026-05-04 00:40:04 +0300

    push

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Laravel API is running',
+        'status' => true,
+    ]);
 });
